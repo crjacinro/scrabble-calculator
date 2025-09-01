9c2f2e4bbbaa230b6165cb19c1dc3dad2ddc9532
Serge Nino Martin Villasica <villasica.serge@gmail.com>
2025-09-01 16:26:17 +0800

    Add timestamp in response

--- backend/src/main/kotlin/org/example/backend/dto/response/TopScoresResponse.kt
+++ backend/src/main/kotlin/org/example/backend/dto/response/TopScoresResponse.kt
@@ -9,11 +9,12 @@ data class TopScoresResponse(
 data class TopScore(
     val wordUsed: String,
     val score: Int,
+    val timestamp: String,
 )
 
 fun List<WordScore>.toResponse(): TopScoresResponse {
     val scores = this.map { score ->
-        TopScore(score.wordUsed, score.score)
+        TopScore(score.wordUsed, score.score, score.playedAt.toString())
     }
 
     return TopScoresResponse(topScores = scores)
